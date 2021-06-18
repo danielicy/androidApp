5e6d996dd6b7be7ca28a1597abf7c8bc9ee240bd
danieli <danielicy@hotmail.com>
2021-06-18 00:19:12 -0700

    geolocation

--- passportCard/src/app/pages/location/components/location/location.component.ts
+++ passportCard/src/app/pages/location/components/location/location.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { android } from '@nativescript/core/application';
+import { GeolocationService } from '../../services/geolocation.service';
+const permissions = require( "nativescript-permissions" );
 
 @Component({
   selector: 'location',
@@ -7,6 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./location.component.css']
 })
 export class LocationComponent  {
+  constructor( private geolocation:GeolocationService) {
+  }
+  
+   getLoation(){
+   // console.log("getting location:");
+   
+     this.geolocation.getLocation()
+    .then(location => {
+      console.log(location.latitude);
+      console.log(location.longitude);
+    });
 
+   
+    //console.log(location);
+    //console.log("got location:");
+  }
+
+  async enableGps(){
+    let gps =await this.geolocation.enableGps();
+   console.log(gps);
+
+  }
+
+ 
 
 }

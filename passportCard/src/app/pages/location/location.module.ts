e2ef38ceb8bca811657228a2879b02e17ec928eb
danieli <danielicy@hotmail.com>
2021-06-17 22:43:30 -0700

    MapComponent

--- passportCard/src/app/pages/location/location.module.ts
+++ passportCard/src/app/pages/location/location.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core'; 
 import { LocationComponent } from './components/location/location.component';
-
+import { MapComponent } from './components/map/map.component'
 import { LocationRoutingModule } from './location-routing.module';
 
 @NgModule({
   declarations: [
-    LocationComponent    
+    LocationComponent,
+    MapComponent    
   ],
   imports: [
     LocationRoutingModule

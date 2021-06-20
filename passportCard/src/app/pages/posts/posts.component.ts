1d06f29aac765272cf34629e128da5b7fa9020ad
danieli <danielicy@hotmail.com>
2021-06-20 14:51:15 +0300

    back

--- passportCard/src/app/pages/posts/posts.component.ts
+++ passportCard/src/app/pages/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from "@angular/common";
 import { Post } from './post';
 import { PostsService } from './posts.service'; 
  
@@ -11,7 +12,8 @@ export class PostsComponent implements OnInit {
 posts: Array<Post>;
 
  
-constructor(private postsService: PostsService) {}
+constructor(private postsService: PostsService,
+  private location: Location) {}
 
 ngOnInit(): void {
    const posts = this.postsService.getAll() .subscribe(
@@ -21,12 +23,13 @@ ngOnInit(): void {
   
 }
 
-delete(post){
-    console.log(post.id);
+  delete(post){
+    
+    this.postsService.delete(post.id);
   }
 
   back(){
-
+    this.location.back();
   }
 
 }
